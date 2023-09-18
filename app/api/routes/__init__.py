6fba18ea17edc5852f27f0f11858ddb01fd91a28
rpadliwinata <rpadliwinata@gmail.com>
2023-09-18 09:13:51 +0700

    feat: htmx

--- app/api/routes/__init__.py
+++ app/api/routes/__init__.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 from .jadwal import router as jadwal_router
 from .asprak import router as asprak_router
+from .htmx import router as htmx_router
 
 router = APIRouter()
 router.include_router(jadwal_router, prefix="/jadwal")
 router.include_router(asprak_router, prefix="/asprak")
+router.include_router(htmx_router, prefix="/hx")
---
+++ app/api/routes/htmx.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import HTMLResponse
+from yattag import Doc
+from db.models import Jadwal
+from db import db_jadwal as db
+
+router = APIRouter()
+
+@router.get("/courses", response_class=HTMLResponse)
+async def get_courses():
+    doc, tag, text = Doc().tagtext()
+    with tag('div', klass='courses-container col-md-4'):
+        with tag('div', klass='course'):
+            with tag('div', klass='course-preview'):
+                with tag('h6'):
+                    text('PEMROGRAMAN WEB')
+                with tag('h4'):
+                    text('WEBPRO')
+            with tag('div', klass='course-info'):
+                with tag('h5'):
+                    text('Link Penting')
+                with tag('a', href='#'):
+                    with tag('h6'):
+                        text('CONTOH2')
+    return doc.getvalue()
+        
