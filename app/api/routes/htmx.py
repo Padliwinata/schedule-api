65758b5b06a58285981e8cd0c826a379b0a62dea
rpadliwinata <rpadliwinata@gmail.com>
2023-09-18 17:04:32 +0700

    feat: htmx

--- app/api/routes/htmx.py
+++ app/api/routes/htmx.py
@@ -10,18 +10,25 @@ router = APIRouter()
 @router.get("/courses", response_class=HTMLResponse)
 async def get_courses():
     doc, tag, text = Doc().tagtext()
-    with tag('div', klass='courses-container col-md-4'):
-        with tag('div', klass='course'):
-            with tag('div', klass='course-preview'):
+    # with tag('div', klass='courses-container col-md-4'):
+    with tag('div', klass='course'):
+        with tag('div', klass='course-preview'):
+            with tag('h6'):
+                text('PEMROGRAMAN WEB')
+            with tag('h4'):
+                text('WEBPRO')
+        with tag('div', klass='course-info'):
+            with tag('h5'):
+                text('Link Penting')
+            with tag('a', href='#'):
                 with tag('h6'):
-                    text('PEMROGRAMAN WEB')
-                with tag('h4'):
-                    text('WEBPRO')
-            with tag('div', klass='course-info'):
-                with tag('h5'):
-                    text('Link Penting')
-                with tag('a', href='#'):
-                    with tag('h6'):
-                        text('CONTOH2')
+                    text('CONTOH2')
+    return doc.getvalue()
+
+@router.get("/example", response_class=HTMLResponse)
+async def get_example():
+    doc, tag, text = Doc().tagtext()
+    with tag('div', klass='courses-container col-md-4'):
+        text('example')
     return doc.getvalue()
         
